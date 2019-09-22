493f8eae8441ca4a19d827cc5f0631784aebea67
Sergey Bodnar <sergeybodnar34@gmail.com>
2019-09-22 10:54:27 +0300

    First step

---
+++ src/main/java/com/serg287/javacore/chapter01/MyFirstJavaProgram.java
@@ -0,0 +1,7 @@
+package main.java.com.serg287.javacore.chapter01;
+
+public class MyFirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Let's get it started 8)");
+    }
+}
