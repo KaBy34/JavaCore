674f6105ecada59fd880af9b9a97fa1ca4d17243
Sergey Bodnar <sergeybodnar34@gmail.com>
2019-09-22 09:28:37 +0300

    Initial commit of the project

---
+++ src/main/java/com/serg287/javacore/chapter02/Example.java
@@ -0,0 +1,10 @@
+package main.java.com.serg287.javacore.chapter02;
+
+public class Example {
+
+    public static void main(String[] args) {
+
+        System.out.println("Simple Java program.");
+
+    }
+}
