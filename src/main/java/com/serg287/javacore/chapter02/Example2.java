a508dec85dc90131e0d33a6726feec81bbd77444
Sergey Bodnar <sergeybodnar34@gmail.com>
2019-09-22 10:23:59 +0300

    Adding Example2

---
+++ src/main/java/com/serg287/javacore/chapter02/Example2.java
@@ -0,0 +1,18 @@
+package main.java.com.serg287.javacore.chapter02;
+/*
+Это еще один короткий пример программы.
+Присвоить исходному файлу имя "Exaiaple2.java" */
+
+class Example2 {
+    public static void main(String args[]) {
+        int num; // в этой строке кода объявляется переменная с именем num;
+
+        num = 100; // в этой строке кода переменной num
+                   // присваивается значение 100
+
+        System.out.println("Этo переменная num: " + num);
+        num = num * 2;
+        System.out.print("Знaчeниe переменной num * 2 равно ");
+        System.out.println(num);
+    }
+}
