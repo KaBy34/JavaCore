19b8f86b4ee9e260ef30b9e2f0565a1727204227
Sergey Bodnar <sergeybodnar34@gmail.com>
2019-09-24 00:10:49 +0300

    5.6

---
+++ src/main/java/com/serg287/javacore/chapter05/While.java
@@ -0,0 +1,12 @@
+package main.java.com.serg287.javacore.chapter05;
+
+// Продемонстрировать применение оператора цикла while
+public class While {
+    public static void main(String[] args) {
+        int n = 10;
+        while(n > 0) {
+            System.out.println("тaкт " + n);
+            n--;
+        }
+    }
+}
