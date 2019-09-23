5ba390e71cc6d85268cfc96e45bcac95d28cf08e
Sergey Bodnar <sergeybodnar34@gmail.com>
2019-09-23 23:54:38 +0300

    5.5

---
+++ src/main/java/com/serg287/javacore/chapter05/StringSwitch.java
@@ -0,0 +1,21 @@
+package main.java.com.serg287.javacore.chapter05;
+
+// Использовать символьные строки для управления оператором switch
+public class StringSwitch {
+    public static void main(String[] args) {
+        String str = "два";
+        switch(str) {
+            case "один":
+                System.out.println("oдин");
+                break;
+            case "два":
+                System.out.println("двa");
+                break;
+            case "три":
+                System.out.println("тpи");
+                break;
+        default:
+            System.out.println("не совпало");
+        }
+    }
+}
