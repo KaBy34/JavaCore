2beb7043ac3a45685d82e5ba2f28f44df148aa55
Sergey Bodnar <sergeybodnar34@gmail.com>
2019-09-23 20:16:17 +0300

    Adding new classes

---
+++ src/main/java/com/serg287/javacore/chapter03/AutoArray.java
@@ -0,0 +1,10 @@
+package main.java.com.serg287.javacore.chapter03;
+
+// Усовершенствованная версия предыдущей программы Array
+public class AutoArray {
+    public static void main(String[] args) {
+
+        int month_days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30,31};
+        System.out.println("B апреле " + month_days[3] + " дней.");
+    }
+}
