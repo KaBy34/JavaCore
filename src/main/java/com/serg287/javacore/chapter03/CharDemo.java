bde24f28dba8838bfe17ce35b6f278dc866cb168
Sergey Bodnar <sergeybodnar34@gmail.com>
2019-09-23 07:38:50 +0300

    Adding few classes

---
+++ src/main/java/com/serg287/javacore/chapter03/CharDemo.java
@@ -0,0 +1,14 @@
+package main.java.com.serg287.javacore.chapter03;
+
+// Продемонстрировать применение типа данных char
+class CharDemo {
+    public static void main ( String args[]) {
+        char ch1, ch2;
+
+        ch1 = 88; // код символа Х
+        ch2 = 'У';
+
+        System.out.print("chl и ch2: ");
+        System.out.println(ch1 + " " + ch2);
+    }
+}
