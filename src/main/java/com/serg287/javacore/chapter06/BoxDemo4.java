0412e9fe1171fff0f39061ab898ed5525a6cf643
Sergey Bodnar <sergeybodnar34@gmail.com>
2019-09-25 00:44:37 +0300

    6.4

---
+++ src/main/java/com/serg287/javacore/chapter06/BoxDemo4.java
@@ -0,0 +1,39 @@
+package main.java.com.serg287.javacore.chapter06;
+
+//Теперь метод volume ( ) возвращает объем параллелепипеда
+class Вох {
+    double width;
+    double height;
+    double depth;
+
+    // рассчитать и возвратить объем
+    double volume() {
+        return width * height * depth;
+    }
+}
+
+class BoxDemo4 {
+    public static void main(String[] args) {
+        Box mybox1 = new Box();
+        Box mybox2 = new Box();
+        double vol;
+
+        // присвоить значения переменным экземпляра mybox1
+        mybox1.width = 10;
+        mybox1.height = 20;
+        mybox1.depth = 15;
+
+        // присвоить другие значения переменным экземпляра mybox2
+        mybox2.width = 3;
+        mybox2.height = 6;
+        mybox2.depth = 9;
+
+        // получить объем первого параллелепипеда
+        vol = mybox1.volume();
+        System.out.println("Oбъeм равен " + vol);
+
+        // получить объем второго параллелепипеда
+        vol = mybox2.volume();
+        System.out.println("Oбъeм равен " + vol);
+    }
+}
