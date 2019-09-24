26934c9f7cd38303d693e832654b20a50db7decc
Sergey Bodnar <sergeybodnar34@gmail.com>
2019-09-25 01:03:32 +0300

    6.5

---
+++ src/main/java/com/serg287/javacore/chapter06/BoxDemo5.java
@@ -0,0 +1,40 @@
+package main.java.com.serg287.javacore.chapter06;
+
+// В этой программе применяется метод с параметрами
+class Box5 {
+    double width;
+    double height;
+    double depth;
+
+    // рассчитать и возвратить объем
+    double volume() {
+        return width * height * depth;
+    }
+    // установить размеры параллелепипеда
+    void setDim(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+}
+
+
+public class BoxDemo5 {
+    public static void main(String[] args) {
+        Box5 mybox1 = new Box5();
+        Box5 mybox2 = new Box5();
+        double vol;
+
+        // инициализировать каждый экземпляр класса Box5
+        mybox1.setDim(10, 20, 15);
+        mybox2.setDim(3, 6, 9);
+
+        // получить объем первого параллелепипеда
+        vol = mybox1.volume();
+        System.out.println("Объeм равен " + vol );
+
+        // получить объем второго параллелепипеда
+        vol = mybox2.volume();
+        System.out.println("Объeм равен " + vol );
+    }
+}
