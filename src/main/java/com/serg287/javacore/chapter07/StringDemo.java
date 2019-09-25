5b61c1346b544c381f1f6d24d2ecd5a55b00bb7a
Sergey Bodnar <sergeybodnar34@gmail.com>
2019-09-26 01:21:24 +0300

    7.19

---
+++ src/main/java/com/serg287/javacore/chapter07/StringDemo.java
@@ -0,0 +1,14 @@
+package main.java.com.serg287.javacore.chapter07;
+
+// Продемонстрировать применение символьных строк String
+public class StringDemo {
+    public static void main(String[] args) {
+        String strOb1 = "Первая строка";
+        String strOb2 = "Вторая строка";
+        String strObЗ = strOb1 + " и " + strOb2;
+
+        System.out.println(strOb1);
+        System.out.println(strOb2);
+        System.out.println(strObЗ);
+    }
+}
