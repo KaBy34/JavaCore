8ed4d8429fdb9038bc86e721bfa6d615f8db5b8a
Sergey Bodnar <sergeybodnar34@gmail.com>
2019-09-25 20:48:59 +0300

    6.7

---
+++ src/main/java/com/serg287/javacore/chapter06/BoxDemo7.java
@@ -0,0 +1,42 @@
+package main.java.com.serg287.javacore.chapter06;
+
+/*  В данном примере программы для инициализации размеров
+    параллелепипеда в классе Вох применяется параметризированный
+    конструктор
+*/
+
+class Box7 {
+    double width;
+    double height;
+    double depth;
+
+    // Это конструктор класса Box7
+    Box7 (double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+
+    // рассчитать и возвратить объем
+    double volume () {
+        return width * height * depth;
+    }
+}
+class BoxDemo7 {
+    public static void main(String[] args) {
+        // объявить, выделить память и инициализировать объекты типа Вох6
+        Box7 mybox1 = new Box7(10, 20, 15);
+        Box7 mybox2 = new Box7(3, 6, 9);
+
+        double vol;
+
+        // получить объем первого параллелепипеда
+        vol = mybox1.volume();
+        System.out.println("Oбъeм равен " + vol);
+
+        // получить объем второго параллелепипеда
+        vol = mybox2.volume();
+        System.out.println("Oбъeм равен " + vol);
+
+    }
+}
