710d98a87ee3c25db5ea8c3ee563bf5665088f87
Sergey Bodnar <sergeybodnar34@gmail.com>
2019-09-26 01:27:52 +0300

    7.20

---
+++ src/main/java/com/serg287/javacore/chapter07/StringDemo2.java
@@ -0,0 +1,24 @@
+package main.java.com.serg287.javacore.chapter07;
+
+// Продемонстрировать некоторые методы из класса String
+public class StringDemo2 {
+    public static void main(String[] args) {
+        String strOb1 = "Первая строка";
+        String strOb2 = "Вторая строка";
+        String strObЗ = strOb1;
+
+        System.out.println("Длинa строки strOb1: " + strOb1.length());
+
+        System.out.println("Cимвoл по индексу 3 в строке strOb1: " + strOb1.charAt(3));
+
+        if (strOb1.equals(strOb2))
+            System.out.println("strOb1 == strOb2");
+        else
+            System.out.println("strOb1 != strOb2");
+
+        if (strOb1.equals(strObЗ))
+            System.out.println("strOb1 == strObЗ");
+        else
+            System.out.println("strOb1 != strObЗ");
+    }
+}
