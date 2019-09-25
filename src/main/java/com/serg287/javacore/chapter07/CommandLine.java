a9413f9dfbfc0bece40e50b4dbb77756a1f0a48a
Sergey Bodnar <sergeybodnar34@gmail.com>
2019-09-26 01:50:42 +0300

    7.22

---
+++ src/main/java/com/serg287/javacore/chapter07/CommandLine.java
@@ -0,0 +1,9 @@
+package main.java.com.serg287.javacore.chapter07;
+
+// Вывести все аргументы командной строки
+class CommandLine {
+    public static void main(String args[]) {
+        for(int i=0; i<args.length; i++)
+            System.out.println("args[" + i + "]: " + args[i]);
+    }
+}
