383090f25d92167830f4609ca4ae12973ab803a4
Sergey Bodnar <sergeybodnar34@gmail.com>
2019-09-27 01:47:57 +0300

    8.8

---
+++ src/main/java/com/serg287/javacore/chapter08/Override.java
@@ -0,0 +1,36 @@
+package main.java.com.serg287.javacore.chapter08;
+
+// Переопределение метода
+class A2 {
+    int i, j;
+    A2(int a, int b) {
+        i = a;
+        j = b;
+    }
+
+    // вывести содержимое переменных i и j
+    void show() {
+        System.out.println("i и j: " + i + " " + j);
+    }
+}
+
+class B2 extends A2 {
+    int k;
+    B2(int a, int b, int c) {
+        super(a, b);
+        k = c;
+    }
+
+    // вывести содержимое переменной k с помощью метода,
+    // переопределяющего метод show() из класса А2
+    void show() {
+        System.out.println("k: " + k);
+    }
+}
+
+public class Override {
+    public static void main(String[] args) {
+        B2 subOb = new B2(1, 2, 3);
+        subOb.show(); // здесь вызывается метод show() из класса В2
+    }
+}
