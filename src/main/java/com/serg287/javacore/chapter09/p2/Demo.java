d3599fa3bc823307c02b0bc19dace3a6dc758bce
Sergey Bodnar <sergeybodnar34@gmail.com>
2019-09-28 09:22:02 +0300

    9.2 Package p2 Demo

---
+++ src/main/java/com/serg287/javacore/chapter09/p2/Demo.java
@@ -0,0 +1,10 @@
+// Демонстрационный пакет p2
+package main.java.com.serg287.javacore.chapter09.p2;
+
+// получить экземпляры различных классов из пакета р2
+public class Demo {
+    public static void main(String[] args) {
+        Protection2 ob1 = пеw Protection2();
+        OtherPackage ob2 = пеw OtherPackage();
+    }
+}
