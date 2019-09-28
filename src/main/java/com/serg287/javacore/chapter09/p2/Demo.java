ad36691d17b6ce6900a6e9a541b32da7ad437a43
Sergey Bodnar <sergeybodnar34@gmail.com>
2019-09-28 09:27:43 +0300

    9.3 Adding Package p2.Protection2

--- src/main/java/com/serg287/javacore/chapter09/p2/Demo.java
+++ src/main/java/com/serg287/javacore/chapter09/p2/Demo.java
@@ -4,7 +4,7 @@ package main.java.com.serg287.javacore.chapter09.p2;
 // получить экземпляры различных классов из пакета р2
 public class Demo {
     public static void main(String[] args) {
-        Protection2 ob1 = пеw Protection2();
-        OtherPackage ob2 = пеw OtherPackage();
+        Protection2 ob1 = new Protection2();
+        OtherPackage ob2 = new OtherPackage();
     }
 }
---
+++ src/main/java/com/serg287/javacore/chapter09/p2/Protection2.java
@@ -0,0 +1,16 @@
+package main.java.com.serg287.javacore.chapter09.p2;
+
+
+class Protection2 extends main.java.com.serg287.javacore.chapter09.p1.Protection {
+    Protection2() {
+        System.out.println("конструктор , унаследованный иэ другого пакета");
+        // доступно только для данного класса или пакета
+        // System.out.println("n = " + n);
+
+        // доступно только для данного класса
+        // System.out.println("n_pri = " + n_pri);
+
+        System.out.println("n_pro = " + n_pro);
+        System.out.println("n_pub = " + n_pub);
+    }
+}
